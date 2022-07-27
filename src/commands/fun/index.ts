acbdf0f76d20fb0de4fb89626bf0a76de5f77ce5
Shiaupiau <stu43005@gmail.com>
2022-07-27 21:01:49 +0800

    feat(): snowflake command

--- src/commands/fun/index.ts
+++ src/commands/fun/index.ts
@@ -2,9 +2,11 @@ import { AppCommand } from '../command.js';
 import { CodeCommand } from './code.js';
 import { EmoteCommand } from './emote.js';
 import { HitokotoCommand } from './hitokoto.js';
+import { SnowflakeCommand } from './snowflake.js';
 
 export const funCommands: AppCommand[] = [
     new CodeCommand(),
     new HitokotoCommand(),
     new EmoteCommand(),
+    new SnowflakeCommand(),
 ];
---
+++ src/commands/fun/snowflake.ts
@@ -0,0 +1,45 @@
+import {
+    ChatInputCommandInteraction,
+    codeBlock,
+    PermissionsString,
+    SlashCommandBuilder,
+    SnowflakeUtil,
+} from 'discord.js';
+import { InteractionUtils } from '../../utils/interaction-utils.js';
+import { Command, CommandDeferType } from '../command.js';
+
+export class SnowflakeCommand implements Command {
+    public metadata = new SlashCommandBuilder()
+        .setName('snowflake')
+        .setDescription('Gets information about a snowflake.')
+        .addStringOption(builder =>
+            builder.setName('snowflake').setDescription('snowflake').setRequired(true)
+        )
+        .toJSON();
+    public deferType = CommandDeferType.HIDDEN;
+    public requireDev = false;
+    public requireGuild = false;
+    public requireClientPerms: PermissionsString[] = [];
+    public requireUserPerms: PermissionsString[] = [];
+
+    public async execute(intr: ChatInputCommandInteraction): Promise<void> {
+        try {
+            const snowflake = SnowflakeUtil.deconstruct(intr.options.getString('snowflake', true));
+            await InteractionUtils.send(
+                intr,
+                codeBlock(
+                    'fix',
+                    `snowflake: ${snowflake.id}
+timestamp: ${snowflake.timestamp}
+workerId: ${snowflake.workerId}
+processId: ${snowflake.processId}
+increment: ${snowflake.increment}
+epoch: ${snowflake.epoch}`
+                ),
+                true
+            );
+        } catch (error: any) {
+            await InteractionUtils.send(intr, `錯誤：無效的 snowflake`, true);
+        }
+    }
+}
